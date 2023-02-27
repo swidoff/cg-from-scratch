c691b38a20f62ef62b8f0b8df72584c6248cdab7
seth <seth@finsera.com>
2023-02-27 09:45:06 -0800

    Chapter 7: Triangles

--- src/rasterizer/main.rs
+++ src/rasterizer/main.rs
@@ -1,13 +1,25 @@
 use crate::log;
 use crate::vec3::Color;
+use itertools::Itertools;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn rasterizer(canvas_height: usize, canvas_width: usize) -> Vec<u8> {
     let mut canvas = Canvas::new(canvas_height, canvas_width);
     let black = Color::new(0., 0., 0.);
-    canvas.draw_line(&Point::new(-200, -100), &Point::new(240, 120), &black);
-    canvas.draw_line(&Point::new(-50, -200), &Point::new(60, 240), &black);
+    let green = Color::new(0., 255., 0.);
+
+    // Chapter 6
+    // canvas.draw_line(&Point::new(-200, -100), &Point::new(240, 120), &black);
+    // canvas.draw_line(&Point::new(-50, -200), &Point::new(60, 240), &black);
+
+    // Chapter 7
+    let p0 = Point::new(-200, -250);
+    let p1 = Point::new(200, 50);
+    let p2 = Point::new(20, 250);
+    canvas.draw_filled_triangle(&p0, &p1, &p2, &green);
+    canvas.draw_wire_frame_triangle(&p0, &p1, &p2, &black);
+
     canvas.pixels
 }
 
@@ -49,13 +61,49 @@ impl Canvas {
         if (p1.x - p0.x).abs() > (p1.y - p0.y).abs() {
             // line is horizontal-ish
             let (p0, p1) = if p0.x > p1.x { (p1, p0) } else { (p0, p1) };
-            for (x, y) in interpolate(p0.x, p0.y as f32, p1.x, p1.y as f32) {
+            for (x, y) in interpolate(p0.x, p0.y, p1.x, p1.y) {
                 self.put_pixel(x, y as i32, color);
             }
         } else {
             // line is vertical-ish
             let (p0, p1) = if p0.y > p1.y { (p1, p0) } else { (p0, p1) };
-            for (y, x) in interpolate(p0.y, p0.x as f32, p1.y, p1.x as f32) {
+            for (y, x) in interpolate(p0.y, p0.x, p1.y, p1.x) {
+                self.put_pixel(x as i32, y, color);
+            }
+        }
+    }
+
+    fn draw_wire_frame_triangle(&mut self, p0: &Point, p1: &Point, p2: &Point, color: &Color) {
+        self.draw_line(p0, p1, color);
+        self.draw_line(p1, p2, color);
+        self.draw_line(p0, p2, color);
+    }
+
+    fn draw_filled_triangle(&mut self, p0: &Point, p1: &Point, p2: &Point, color: &Color) {
+        // Sort the points by their y location.
+        let mut points = [p0, p1, p2];
+        points.sort_by_key(|&p| p.y);
+        let [p0, p1, p2] = points;
+
+        // Find the points along the sides of the triangle.
+        let x01 = interpolate(p0.y, p0.x, p1.y, p1.x);
+        let x02 = interpolate(p0.y, p0.x, p2.y, p2.x).collect_vec(); // Long size
+        let x12 = interpolate(p1.y, p1.x, p2.y, p2.x);
+
+        // Concatenate the two short sides.
+        let x012 = x01.dropping(1).chain(x12).collect_vec();
+
+        // Determine which side is the left and which is the right.
+        let midpoint = x02.len() / 2;
+        let (left_side, right_side) = if x02[midpoint].0 < x012[midpoint].0 {
+            (x02, x012)
+        } else {
+            (x012, x02)
+        };
+
+        // Draw the horizontal line segments.
+        for (i, &(y, x_left)) in left_side.iter().enumerate() {
+            for x in (x_left as i32)..(right_side[i].1 as i32 + 1) {
                 self.put_pixel(x as i32, y, color);
             }
         }
@@ -73,14 +121,14 @@ impl Point {
     }
 }
 
-fn interpolate(i0: i32, d0: f32, i1: i32, d1: f32) -> impl Iterator<Item = (i32, f32)> {
+fn interpolate(i0: i32, d0: i32, i1: i32, d1: i32) -> impl Iterator<Item = (i32, f32)> {
     let a = if i0 == i1 {
         0.
     } else {
-        (d1 - d0) / ((i1 - i0) as f32)
+        ((d1 - d0) as f32) / ((i1 - i0) as f32)
     };
 
-    (i0..(i1 + 1)).scan(d0, move |d, i| {
+    (i0..(i1 + 1)).scan(d0 as f32, move |d, i| {
         let res = Some((i, *d));
         *d += a;
         res
